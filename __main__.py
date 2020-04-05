7aefc53f24b3315d920edd5d61df9e550fdd9b01
jpricardo <esse.email.memo@gmail.com>
2020-04-05 01:57:37 -0300

    classes updates

--- __main__.py
+++ __main__.py
@@ -5,8 +5,23 @@ import classes as cl
 
 
 teste = cl.NPC('Testinho', 100)
-print(teste.__dict__)
 teste.set_friendly(False)
 print(teste.__dict__)
-teste.kill()
-print(teste.__dict__)
+
+testinho = cl.Enemy('Doente', 50)
+testinho.set_drop(drop=['espada', 'bosta'], drop_rate=[0.01, 0.5])
+print(testinho.__dict__)
+
+def create_character():
+	name = input('Nome: ')
+	hp = int(input('HP: '))
+	c = cl.NPC(name, hp)
+
+	return c
+
+
+
+
+
+test = create_character()
+print(test.__dict__)
--- classes.py
+++ classes.py
@@ -4,7 +4,6 @@
 
 class Character():
 
-
 	def __init__(self, name, hp, alive=True):
 		self.name = name
 		self.hp = int(hp)
@@ -14,13 +13,24 @@ class Character():
 		self.hp = 0
 		self.alive = False
 
+	def set_drop(self, drop=[], drop_rate=[]):
+	
+		self.drop = []
+		for i in range(len(drop)):
+			self.drop.append([drop[i], drop_rate[i]])
 
-
-
+	def drop_loot(self):
+		pass
 
 
 class NPC(Character):
 	
-
 	def set_friendly(self, friendly):
 		self.friendly = friendly
+
+
+class Enemy(Character):
+	pass
+	
+
+
